e13811db9084794de087414199227ac1ccc8b0f6
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-10-14 12:02:11 +0200

    init

---
+++ occurence/occurence-starter.js
@@ -0,0 +1,21 @@
+let p = `
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu euismod purus, eu scelerisque lorem. Quisque elementum nulla ipsum, vitae sollicitudin magna malesuada at. Integer dignissim odio nec est hendrerit venenatis. Nam ac lacus ut lectus dignissim mollis. Nullam vitae tortor at quam elementum fermentum. Praesent tempus, magna vitae tincidunt sodales, libero leo ornare augue, sit amet mollis dolor quam et elit. Curabitur eleifend, augue eu interdum imperdiet, ipsum velit ultricies neque, ac pulvinar risus tortor ut metus. Suspendisse iaculis mauris sapien, id ullamcorper lorem condimentum quis. Pellentesque tristique lorem leo, sit amet maximus nunc condimentum et. Donec dignissim nisl sit amet eros gravida faucibus. Morbi dapibus lorem in rhoncus luctus. Nullam suscipit lorem ut arcu vehicula vulputate. Ut posuere massa at vehicula tristique. Integer a iaculis velit, eget feugiat odio.
+Quisque non dolor non mauris finibus varius vel id nisl. Aenean feugiat ipsum in pellentesque faucibus. Donec odio lacus, porta vitae ullamcorper et, vehicula a nibh. Suspendisse dictum diam id scelerisque dignissim. Phasellus eu elit in tellus porttitor efficitur. Vivamus scelerisque rhoncus iaculis. Fusce laoreet cursus nisl ac tincidunt. Pellentesque rhoncus erat quis arcu cursus, sit amet vehicula neque mattis.
+
+Aliquam vel nibh enim. Quisque maximus vel nisi et vulputate. Sed tempus ullamcorper lectus, imperdiet mattis risus commodo vitae. Donec ex arcu, malesuada non nunc quis, consequat efficitur arcu. Cras ex lacus, viverra sit amet nisi at, tincidunt lacinia leo. Donec consectetur lacus maximus purus gravida, in vulputate enim egestas. Nam aliquet nec ligula eget porttitor. Quisque dictum quam at scelerisque ultricies. Aenean in laoreet turpis, eget mollis lorem.
+`
+
+/********  EXEMPLE */
+/*
+[
+  { word: 'posuere', occurences: 2 },
+  { word: 'massa', occurences: 2 },
+  { word: 'tristique.', occurences: 2 },
+  { word: 'a', occurences: 3 },
+  { word: 'velit,', occurences: 2 },
+  { word: 'eget', occurences: 4 },
+  { word: 'feugiat', occurences: 3 },
+  { word: 'odio.\n\nQuisque', occurences: 2 },
+  ...
+]
+*/
---
+++ palindrome/palindrome-starter.js
@@ -0,0 +1,14 @@
+let words = [
+    "kayak",
+    "Paris",
+    "été",
+    "Ressasser"
+]
+
+/******** Result : ********/
+/*
+kayak est un palindrome
+Paris n'est pas un palindrome
+été est un palindrome
+Ressasser est un palindrome
+*/
