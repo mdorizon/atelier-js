86d2070223da4e4bcf2da34fb0993daff7fca4dc
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-10-14 12:16:18 +0200

    init

---
+++ moyenne/script.js
@@ -0,0 +1,6 @@
+// demander une liste de notes
+// affciher la moyenne des notes
+// afficher la note la plus haute
+// afficher la note la plus basse
+// afficher la note la plus proche de 10
+// afficher la note la plus proche de 0
