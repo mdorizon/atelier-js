23fc0936a6f9e66fefb1065af95984a00afd0c2e
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-11-04 11:06:16 +0100

    update

--- moyenne/script.js
+++ moyenne/script.js
@@ -1,4 +1,13 @@
-// demander une liste de notes
+// OPTION 1
+// Tant que l'utilisateur veut ajouter une noxte
+    // On lui demande de saisir une note (prompt)
+    // ajouté la note à la liste de notes (tableau)
+    // On lui demande s'il souhaite ajouter une autre note (confirm)
+
+// OPTION 2
+// Demander à l'utilisateur de saisir une liste de notes séparées par des virgules
+// On transforme la chaine de caractères en tableau avec la méthode split
+
 // affciher la moyenne des notes
 // afficher la note la plus haute
 // afficher la note la plus basse
