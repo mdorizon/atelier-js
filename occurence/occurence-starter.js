61dde763737a44c6022d227a5ebabe7f66a5eeee
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-11-11 12:08:32 +0100

    update

--- occurence/occurence-starter.js
+++ occurence/occurence-starter.js
@@ -9,13 +9,21 @@ Les faire frire 2/3 mn puis les égoutter sur du papier absorbant.
 Servir chaud ou froid avec des petites sauces tomates aux herbes, ou sauces yaourts.
 `
 // compter le nombre d'occurence pour chaque mot
+// La casse n'est pas prise en compte
 /********  EXEMPLE */
 /*
 [
   { word: 'chiches', occurences: 2 },
   { word: 'les', occurences: 10 },
   { word: 'pois.', occurences: 2 },
-  { word: 'et', occurences: 9 },
+  { word: 'et', occurences: 5 },
   ...
 ]
 */
+
+// Bonus : trier les mots par ordre alphabétique
+// Bonus : trier les mots par ordre décroissant du nombre d'occurence
+// Bonus : afficher les 10 mots les plus utilisés
+// Bonus : afficher les 10 mots les moins utilisés
+// Bonus : afficher les 10 mots les plus longs
+// Bonus : afficher les 10 mots les plus courts
