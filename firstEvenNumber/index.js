fd3bee24b1b8dfab43e86e98787f97203ef8bb5d
MDorizon <mdorizon@proton.me>
2024-10-23 11:06:33 +0200

    :sparkles: firstEvenNumber

--- firstEvenNumber/index.js
+++ firstEvenNumber/index.js
@@ -2,4 +2,13 @@
 
 
 const numbers = [1, 3, 7, 8, 10];
-findFirstEven(numbers); // 8
+
+function findFirstEven(array) {
+    for(i = 0; array.length; i++) {
+        if (array[i] % 2 == 0) {
+            return array[i]
+        }
+    }
+}
+
+console.log(findFirstEven(numbers)); // 8
