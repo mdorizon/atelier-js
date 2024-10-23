fd700895d65458d3187ba29aaf1c527a8289ea52
MDorizon <mdorizon@proton.me>
2024-10-23 11:17:12 +0200

    :sparkles: uniqueValueArray

--- uniqueValueArray/index.js
+++ uniqueValueArray/index.js
@@ -1,5 +1,14 @@
 const arr = [1, 2, 3, 4, 4, 5, 6, 6, 7];
 
+function uniqueValues(arr) {
+    const newArray = []
+    arr.forEach(el => {
+        if (!newArray.includes(el)) {
+            newArray.push(el);
+        }
+    })
+    return newArray
+}
 
 // Écris une fonction qui prend un tableau en argument et retourne un tableau contenant uniquement des valeurs uniques.
-uniqueValues(arr); // [1, 2, 3, 4, 5, 6, 7]
+console.log(uniqueValues(arr)); // [1, 2, 3, 4, 5, 6, 7]
